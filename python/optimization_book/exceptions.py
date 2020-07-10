b9ae7e1cddadc4632c07955d640a2c92bb74821f
michelbierlaire <michel.bierlaire@epfl.ch>
2020-07-10 15:33:41 +0200

    Create exceptions.py

---
+++ python/optimization_book/exceptions.py
@@ -0,0 +1,13 @@
+"""Defines a generic exception for the package
+
+:author: Michel Bierlaire
+
+:date: Fri Jul 10 15:09:09 2020
+
+"""
+
+# Too constraining
+# pylint: disable=invalid-name
+
+class optimizationError(Exception):
+    """ Defines a generic exception. """
