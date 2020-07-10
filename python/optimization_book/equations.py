6c1189dfbcef123bab71050e49bccca54c25a1f9
michelbierlaire <michel.bierlaire@epfl.ch>
2020-07-10 17:01:18 +0200

    Update equations.py

--- python/optimization_book/equations.py
+++ python/optimization_book/equations.py
@@ -65,3 +65,49 @@ def newtonEquationOneVariable(fct, x0, eps, maxiters=100):
         return x, iters, f'Required precision has been reached: {np.abs(f)} <= {eps}'
 
     return None, iters, f'Maximum number of iterations reached: {maxiters}'
+
+def newtonSeveralVariables(fct, x0, eps, maxiters=100):
+    """
+    :param fct: function that returns the value of the function and its Jacobian
+    :type fct: function
+
+    :param x0: starting point for the algorithm.
+    :type x0: numpy.array
+
+    :param eps: precision to reach.
+    :type eps: float.
+
+    :param maxiters: maximum number of iterations. Default: 100.
+    :type maxiters: int
+
+    :return: x, iters, status where
+
+                 - x is solution found, or None is
+                         unsuccessful,
+                 - iters contains a list of
+                         tuple. For each iteration, the number of the
+                         iteration, the value of x, the value of f and
+                         the value of g,
+                 - status constains a message describing the reason why the algorithm stopped.
+    :rtype: float, list(int, np.array 1D, np.array 1D, np.array 2D), str
+    """
+    k = 0
+    x = x0
+    f, J = fct(x)
+    iters = list()
+    iters.append([k, x, f, J])
+    while np.linalg.norm(f) > eps and k <= maxiters:
+        k += 1
+        try:
+            d = np.linalg.solve(J, -f)
+            x = x + d
+        except np.linalg.LinAlgError as e:
+            message = f'Numerical issue encountered in iteration {k}: {e}'
+            return None, message
+        f, J = fct(x)
+        iters.append([k, x, f, J])
+
+    if np.linalg.norm(f) <= eps:
+        return x, iters, f'Required precision has been reached: {np.linalg.norm(f)} <= {eps}'
+
+    return None, iters, f'Maximum number of iterations reached: {maxiters}'
