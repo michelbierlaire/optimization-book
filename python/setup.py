d91bac5a94b4fc4cf189a7317dbd26d4cba22fbb
michelbierlaire <michel.bierlaire@epfl.ch>
2020-07-10 15:33:26 +0200

    Create setup.py

---
+++ python/setup.py
@@ -0,0 +1,10 @@
+import setuptools 
+
+setuptools.setup(name='optimization_book',
+                 version='0.1',
+                 description=('Algorithms described in the book Bierlaire (2015) '
+                              'Optimization: principles and algorithms, EPFL Press'),
+                 url='http://github.com/optimization-book/python',
+                 author='Michel Bierlaire',
+                 author_email='michel.bierlaire@epfl.ch',
+                 packages=setuptools.find_packages())
